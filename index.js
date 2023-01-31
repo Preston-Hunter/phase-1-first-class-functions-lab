4acb3eba630a1cce119627d2bdcddc0f39c699af
PrestonHunter <prestonhunter2018@gmail.com>
2023-01-31 16:16:37 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(d){
+    if (d.length >= 2){
+        return d.slice(0,2);
+    }
+    else{
+        return d;
+    }
+}
+
+function returnLastTwoDrivers(d){
+    if (d.length >= 2){
+        return d.slice(-2);
+    }
+    else{
+        return d;
+    }
+}
+
+let selectingDrivers = [(returnFirstTwoDrivers), returnLastTwoDrivers];
+
+function createFareMultiplier(int){
+    return function (fare){return fare * int};
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+
+function selectDifferentDrivers(names, twoDrivers){
+    return twoDrivers(names);
+}
+
+fareDoubler(10)
